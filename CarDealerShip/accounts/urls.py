65c27e1021da8704496b51b27e9bc4ce8a2d6416
KrissTemelkov <whatmaneger@gmail.com>
2022-12-13 23:27:31 +0200

    accounts finish

--- CarDealerShip/accounts/urls.py
+++ CarDealerShip/accounts/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
-from CarDealerShip.accounts.views import profile_info
+from CarDealerShip.accounts.views import SignInView, SignUpView, SignOutView, UserDetailsView, UserEditView, \
+    UserDeleteView
 
 urlpatterns = (
-    path('', profile_info, name='accountInfo'),
+    path('login/', SignInView.as_view(), name='login user'),
+    path('register/', SignUpView.as_view(), name='register user'),
+    path('logout/', SignOutView.as_view(), name='logout user'),
+    path('profile/<int:pk>/', include([
+        path('', UserDetailsView.as_view(), name='details user'),
+        path('edit/', UserEditView.as_view(), name='edit user'),
+        path('delete/', UserDeleteView.as_view(), name='delete user')
+    ])),
 )
