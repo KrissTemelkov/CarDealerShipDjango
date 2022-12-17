641f7c50a2503681b145b94daf649f96c93fbdb7
KrissTemelkov <whatmaneger@gmail.com>
2022-12-17 23:30:29 +0200

    final

--- CarDealerShip/common/admin.py
+++ CarDealerShip/common/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from CarDealerShip.common.forms import CarEditForm, CarCreateForm
 from CarDealerShip.common.models import Car
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    pass
+    form = CarEditForm
+    add_form = CarCreateForm
